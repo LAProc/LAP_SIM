6d7a788812de90376b698ce9bd5eb38677d16299
Ardavan Pedram <ardavan@Unicron.local>
2014-12-23 08:58:28 -0800

    copyright update to 3 phase

--- src/IO.cpp
+++ src/IO.cpp
@@ -2,9 +2,9 @@
    Copyright (c) 2010-2015, Ardavan Pedram and The University of Texas at Austin
    All rights reserved.
    
-   This file is part of LAP_SIM and is under the BSD 2-Clause License, 
+   This file is part of LAP_SIM and is under the BSD 3-Clause License, 
    which can be found in the LICENSE file in the root directory, or at 
-   http://opensource.org/licenses/BSD-2-Clause
+   http://opensource.org/licenses/BSD-3-Clause
 */
 
 #include "IO.h"
